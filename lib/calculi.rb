d409d143dce6739ea4309001e695d93522b60cca
Noah Ryan Holt <noahryanholt@tutanota.de>
2016-11-13 21:31:36 -0600

    Added .gitignore and finished very rough draft of calculi

--- lib/calculi.rb
+++ lib/calculi.rb
@@ -1,6 +1,3 @@
-$symbol_table = {'numbers' => [1,2,3,4,5,6,7,8,9], 'operators' => ['+', '-', '*', '/', '^', '%', '>', '<']}
-$infix_operator_precendence = {'+' => 2, '-' => 2, '*' => 3, '/' => 3, '^' => 4, '(' => 5, ')' => 5}
-
 class Calculi
         def initialize(string, type)
                 @string = string
@@ -10,35 +7,35 @@ class Calculi
         def parse
                 case @type
                 when 'pn' || 'rpn'
-                        @array = math_notation_parse(@string)
+                        @array = MathNotation.new(@string, @type).parse
                 when 'lisp' || 'reverse_lisp'
-                        @array = lisp_parse(@string)
+                        @array = Lisp.new(@string, @type).parse
                 end
         end
 
         def indices_finder
                 case @type
                 when 'pn' || 'rpn'
-                       @indices = math_notation_indices_finder(array, type)
+                        @indices = MathNotation.new(@array, @type).indices_finder
                 when 'lisp' || 'reverse-lisp'                 
-                       @indices = lisp_indices_finder(array)
+                        @indices = Lisp.new(@array, @type).indices_finder
                 end
         end
-        
+
         def eval
                 case @type
                 when 'pn' || 'rpn'
-                        math_notation_eval(@array, @type, @indices)
+                        MathNotation.new(@string, @type).eval
                 when 'lisp' || 'reverse-lisp'
-                        lisp_eval(@array, @type, @indices)
+                        Lisp.new(@string, @type).eval
                 end
         end
 end
 
 class String
-        def is_numeric?(s)
+        def is_numeric?
                 begin
-                        Float(s)
+                        Float(self)
                 rescue
                         false
                 else 
@@ -55,77 +52,100 @@ class Lisp
 
         def parse
                 @array = @string.split(' ')
-                @array = @array.map{|x| is_numeric?(x) == false && x.length > 1 ? x.split('') : x}.flatten
-                @array = @array.map{|x| is_numeric?(x) != false ? x.to_f : x}
+                @array = @array.map{|x| x.is_numeric? == false && x.length > 1 ? x.split('') : x}.flatten
+                @array = @array.map{|x| x.is_numeric? != false ? x.to_f : x}
+        end
+
+        def execute_lisp_array(array)
+                array[2..-2].reduce(array[1].to_sym)
+        end
+
+        def execute_reverse_lisp_array(array)
+                array[0..-2].reduce(array[-1].to_sym)
+        end
+
+        def execute(array)
+                case @type
+                when 'lisp'
+                        execute_lisp_array(array)
+                when 'reverse-lisp'
+                        execute_reverse_lisp_array(array)
+                end
         end
 
         def indices_finder
-                @indices = array.length.times.select{|i| array[i] == "(" || array[i] == ")"}
+                self.parse
+                @indices = @array.length.times.select{|i| @array[i] == "(" || @array[i] == ")"}
+                @parenthesis_pairs = []
+                while @indices.length > 0
+                        @parenthesis_pairs.unshift([@indices.shift, @indices.pop])
+                return @parenthesis_pairs
+                end
         end
 
         def eval
-                @parenthesis_pairs = []
-                while @indices.length > 0
-                        @parenthesis_pairs.unshift(@indices.shift, @indices.pop)
-                end
-                @parenthesis_pairs.map{|l, r| @array.insert(l, eval_subarray_executor(@array.slice!(@array[l..r])))}
+                self.indices_finder
+                case @type
+                        when 'lisp'
+                                @parenthesis_pairs.map{|l, r| @array.insert(l, execute(@array.slice!(l..r)))}
+                        when 'reverse-lisp'
+                                @parenthesis_pairs.map{|l, r| @array.insert(r, execute(@array.slice!(l..r)))}
+                        end
+                return @array[0]
         end        
 end
 
-class Math-Notation 
+class MathNotation 
         def initialize(string, type)
                 @string = string
                 @type = type
                 @num_elements = {"pn" => 3, "rpn" => -3}
+                @symbol_table = {'numbers' => [1,2,3,4,5,6,7,8,9], 'operators' => ['+', '-', '*', '/', '^', '%', '>', '<']}
+                @infix_operator_precendence = {'+' => 2, '-' => 2, '*' => 3, '/' => 3, '^' => 4, '(' => 5, ')' => 5}
         end
 
         def parse
                 @array = @string.split(' ')
-                @array = @array.map{|x| is_numeric?(x) != false ? x.to_f : x}
+                @array = @array.map{|x| x.is_numeric? != false ? x.to_f : x}
         end
 
+        def execute(array)
+                case @type
+                when 'pn'
+                        execute_pn_array(*array)
+                when 'rpn' 
+                        execute_rpn_array(*array)
+                when 'infix'
+                        execute_infix_array(*array)
+                end
+        end 
+
+        def execute_pn_array(operator, num1, num2)
+                num1.method(operator).call(num2)
+        end
+
+        def execute_rpn_array(num2, num1, operator)
+                num2.method(operator).call(num1)
+        end
+
+        def execute_infix_array(num1, operator, num2)
+                num1.method(operator).call(num2)
+        end                
+
         def indices_finder
-                @indices = @array.length.times.select{|i| $symbol_table['operators'].include?(@array[i])}.reverse 
+                @indices = @array.length.times.select{|i| @symbol_table['operators'].include?(@array[i])}.reverse 
         end
 
         def eval
-                @indices.map{|i| @array.insert(i, eval_subarray_executor(@array.slice!(i, num_elements[type]), type))}
+                self.parse
+                self.indices_finder
+                @indices.map{|i| @array.insert(i, execute(@array.slice!(i, @num_elements[@type])))}
                 return @array[0]
         end
 end
 
-
-def eval_subarray_executor(array, type)
-        case type
-        when 'pn'
-                eval_pn_array(*array)
-        when 'rpn'
-                eval_rpn_array(*array)
-        when 'infix'
-                eval_infix_array(*array)
-        when 'lisp'
-                eval_lisp_array(*array)
-        when 'reverse-lisp'
-                eval_reverse_lisp_array(*array)
+class REPL
+        def initialize(prompt)
+                @prompt = prompt
         end
-end
-
-def eval_pn_array(operator, num1, num2)
-        num1.method(operator).call(num2)
-end
-
-def eval_rpn_array(num1, num2, operator)
-        num2.method(operator).call(num1)
-end
-
-def eval_infix_array(num1, operator, num2)
-        num1.method(operator).call(num2)
-end
-
-def eval_lisp_array(array)
-        array[2..-2].reduce(array[1].to_sym)
-end
-
-def eval_reverse_lisp_array(array)
-        array[1..-3].reduce(array[-2].to_sym)
 end
